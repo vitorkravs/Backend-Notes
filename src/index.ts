e8948cd8d063bfc433f6e93919cc16b1cb781d4f
vitor kravszenko <vitorkravs3@gmail.com>
2024-01-05 12:47:17 -0300

    feat: permitindo solicitações

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ import "./config/DbConfig";
 const app = express();
 const PORT = 3333;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Permitir solicitações
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use(routes);
 
