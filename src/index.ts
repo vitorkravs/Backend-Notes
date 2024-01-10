686e8c462caf08c367117af565a49a3eaa9a224b
vitor kravszenko <vitorkravs3@gmail.com>
2024-01-10 16:03:48 -0300

    feat: get api test deletion

--- src/index.ts
+++ src/index.ts
@@ -19,11 +19,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(routes);
 
-app.get("/", (request: Request, response: Response) => {
-  const data = [{ name: "vitor" }, { profissao: "engenheiro" }];
-  return response.json(data);
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
